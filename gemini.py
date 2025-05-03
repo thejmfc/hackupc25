675d906acc4039cf1edb39c9dced7fe5606b9558
Euan Jones <joneseuan28@gmail.com>
2025-05-04 00:36:24 +0100

    Updating files

--- gemini.py
+++ gemini.py
@@ -1,9 +1,6 @@
 from google import genai
 import urllib.request, json
 
-import database
-
-
 def fetch_travel_recommendations(code):
 
     data = ""
@@ -45,10 +42,11 @@ def fetch_travel_recommendations(code):
     
     """
 
-    for entry in database.FormEntry.objects:
-        if entry.group_code == code:
-            prompt += entry.to_json()
-            prompt += "\n"
+    with urllib.request.urlopen(f"https://script.google.com/macros/s/AKfycbzxvYh-HEpUz7xPXdK3S1jZ5pZYbc5D72jRRPUm8g46n4Z7RnqGscVWpkk1UcMqd9QHkg/exec?group_code={code}") as url:
+        data = json.load(url)
+
+    prompt += str(data)
+    # print(prompt)
 
     client = genai.Client(api_key="AIzaSyBxsgfGzrQuCVOlLPaANHIN2dVfuMm5Hyk")
 
@@ -60,10 +58,9 @@ def fetch_travel_recommendations(code):
 
     json_data = json.loads(clean)
 
-
-    dest_airport = json_data["airport"]
-    outbound = json_data["outbound"]
-    inbound = json_data["inbound"]
+    # dest_airport = json_data["airport"]
+    # outbound = json_data["outbound"]
+    # inbound = json_data["inbound"]
 
     return {
         "original_data": data,
