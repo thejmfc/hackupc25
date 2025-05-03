89f0ed65c25d0f151ac9e3d121e3024b3dc1aa98
Jack Gledhill <me@jackgledhill.com>
2025-05-03 20:47:25 +0200

    Get form insertion/fetching working

--- database/form_entry.py
+++ database/form_entry.py
@@ -13,18 +13,18 @@ class FormEntry(Document):
         phone_number (str):   User's phone number, used by Twilio
         airport (str):        Airport the user would like to fly from
         prefs (list):         List of freeform user preferences, contains info like desired activities, etc
-        date (datetime):      Rough date the user would like the trip to be
+        date (str):           Rough date the user would like the trip to be
         length (int):         Desired length of trip in days
     """
 
     meta = {"collection": "forms"}
 
     # Document field definitions
-    timestamp: datetime = DateTimeField(required=True)
+    timestamp: datetime = DateTimeField(default=datetime.now())
     group_code: str = StringField(required=True)
     name: str = StringField(required=True)
     phone_number: str = StringField(required=True)
     airport: str = StringField(required=True)
     prefs: list[str] = ListField(StringField(), required=True)
-    date: datetime = DateTimeField(required=True)
+    date: str = StringField(required=True)
     length: int = IntField(required=True)
